2dae873af9b1de569e68868ab4792ef41f35ce48
Yao Lu <luyaocns@gmail.com>
2021-09-07 16:42:58 +0800

    update tfrank (unfinished)

--- src/rank/tfrank.py
+++ src/rank/tfrank.py
@@ -1,6 +1,4 @@
-#!/usr/bin/env python
-
-# # Copyright 2019 The TensorFlow Ranking Authors.
+# Copyright 2021 The TensorFlow Ranking Authors.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -61,6 +59,11 @@ tensorflow_ranking/examples/tf_ranking_libsvm_py_binary && \
 --num_features=136
 
 You can use TensorBoard to display the training results stored in $OUTPUT_DIR.
+
+Notes:
+  * Use --alsologtostderr if the output is not printed into screen.
+  * In addition, you can enable multi-objective learning by adding the following
+  flags: --secondary_loss=<the secondary loss key>.
 """
 
 from absl import flags
@@ -88,12 +91,26 @@ flags.DEFINE_integer("list_size", 100, "List size used for training.")
 flags.DEFINE_integer("group_size", 1, "Group size used in score function.")
 
 flags.DEFINE_string("loss", "pairwise_logistic_loss",
-                    "The RankingLossKey for loss function.")
+                    "The RankingLossKey for the primary loss function.")
+flags.DEFINE_string(
+    "secondary_loss", None, "The RankingLossKey for the secondary loss for "
+    "multi-objective learning.")
+flags.DEFINE_float(
+    "secondary_loss_weight", 0.5, "The weight for the secondary loss in "
+    "multi-objective learning.")
 
 FLAGS = flags.FLAGS
 
+_PRIMARY_HEAD = "primary_head"
+_SECONDARY_HEAD = "secondary_head"
 
-class IteratorInitializerHook(tf.train.SessionRunHook):
+
+def _use_multi_head():
+  """Returns True if using multi-head."""
+  return FLAGS.secondary_loss is not None
+
+
+class IteratorInitializerHook(tf.estimator.SessionRunHook):
   """Hook to initialize data iterator after session is created."""
 
   def __init__(self):
@@ -110,8 +127,9 @@ def example_feature_columns():
   """Returns the example feature columns."""
   feature_names = ["{}".format(i + 1) for i in range(FLAGS.num_features)]
   return {
-      name: tf.feature_column.numeric_column(
-          name, shape=(1,), default_value=0.0) for name in feature_names
+      name:
+      tf.feature_column.numeric_column(name, shape=(1,), default_value=0.0)
+      for name in feature_names
   }
 
 
@@ -128,7 +146,7 @@ def load_libsvm_data(path, list_size):
     features = {k: float(v) for (k, v) in kv_pairs}
     return qid, features, label
 
-  tf.logging.info("Loading data from {}".format(path))
+  tf.compat.v1.logging.info("Loading data from {}".format(path))
 
   # The 0-based index assigned to a query.
   qid_to_index = {}
@@ -136,7 +154,7 @@ def load_libsvm_data(path, list_size):
   qid_to_ndoc = {}
   # Each feature is mapped an array with [num_queries, list_size, 1]. Label has
   # a shape of [num_queries, list_size]. We use list for each of them due to the
-  # unknown number of quries.
+  # unknown number of queries.
   feature_map = {k: [] for k in example_feature_columns()}
   label_list = []
   total_docs = 0
@@ -160,13 +178,15 @@ def load_libsvm_data(path, list_size):
         discarded_docs += 1
         continue
       for k, v in six.iteritems(features):
-        assert k in feature_map, "Key {} not founded in features.".format(k)
+        assert k in feature_map, "Key {} not found in features.".format(k)
         feature_map[k][batch_idx][doc_idx, 0] = v
       label_list[batch_idx][doc_idx] = label
 
-  tf.logging.info("Number of queries: {}".format(len(qid_to_index)))
-  tf.logging.info("Number of documents in total: {}".format(total_docs))
-  tf.logging.info("Number of documents discarded: {}".format(discarded_docs))
+  tf.compat.v1.logging.info("Number of queries: {}".format(len(qid_to_index)))
+  tf.compat.v1.logging.info(
+      "Number of documents in total: {}".format(total_docs))
+  tf.compat.v1.logging.info(
+      "Number of documents discarded: {}".format(discarded_docs))
 
   # Convert everything to np.array.
   for k in feature_map:
@@ -181,14 +201,28 @@ def get_train_inputs(features, labels, batch_size):
   def _train_input_fn():
     """Defines training input fn."""
     features_placeholder = {
-        k: tf.placeholder(v.dtype, v.shape) for k, v in six.iteritems(features)
+        k: tf.compat.v1.placeholder(v.dtype, v.shape)
+        for k, v in six.iteritems(features)
     }
-    labels_placeholder = tf.placeholder(labels.dtype, labels.shape)
-    dataset = tf.data.Dataset.from_tensor_slices((features_placeholder,
-                                                  labels_placeholder))
+    if _use_multi_head():
+      placeholder = tf.compat.v1.placeholder(labels.dtype, labels.shape)
+      labels_placeholder = {
+          _PRIMARY_HEAD: placeholder,
+          _SECONDARY_HEAD: placeholder,
+      }
+    else:
+      labels_placeholder = tf.compat.v1.placeholder(labels.dtype, labels.shape)
+    dataset = tf.data.Dataset.from_tensor_slices(
+        (features_placeholder, labels_placeholder))
     dataset = dataset.shuffle(1000).repeat().batch(batch_size)
-    iterator = dataset.make_initializable_iterator()
-    feed_dict = {labels_placeholder: labels}
+    iterator = tf.compat.v1.data.make_initializable_iterator(dataset)
+    if _use_multi_head():
+      feed_dict = {
+          labels_placeholder[head_name]: labels
+          for head_name in labels_placeholder
+      }
+    else:
+      feed_dict = {labels_placeholder: labels}
     feed_dict.update(
         {features_placeholder[k]: features[k] for k in features_placeholder})
     iterator_initializer_hook.iterator_initializer_fn = (
@@ -205,13 +239,27 @@ def get_eval_inputs(features, labels):
   def _eval_input_fn():
     """Defines eval input fn."""
     features_placeholder = {
-        k: tf.placeholder(v.dtype, v.shape) for k, v in six.iteritems(features)
+        k: tf.compat.v1.placeholder(v.dtype, v.shape)
+        for k, v in six.iteritems(features)
     }
-    labels_placeholder = tf.placeholder(labels.dtype, labels.shape)
-    dataset = tf.data.Dataset.from_tensors((features_placeholder,
-                                            labels_placeholder))
-    iterator = dataset.make_initializable_iterator()
-    feed_dict = {labels_placeholder: labels}
+    if _use_multi_head():
+      placeholder = tf.compat.v1.placeholder(labels.dtype, labels.shape)
+      labels_placeholder = {
+          _PRIMARY_HEAD: placeholder,
+          _SECONDARY_HEAD: placeholder,
+      }
+    else:
+      labels_placeholder = tf.compat.v1.placeholder(labels.dtype, labels.shape)
+    dataset = tf.data.Dataset.from_tensors(
+        (features_placeholder, labels_placeholder))
+    iterator = tf.compat.v1.data.make_initializable_iterator(dataset)
+    if _use_multi_head():
+      feed_dict = {
+          labels_placeholder[head_name]: labels
+          for head_name in labels_placeholder
+      }
+    else:
+      feed_dict = {labels_placeholder: labels}
     feed_dict.update(
         {features_placeholder[k]: features[k] for k in features_placeholder})
     iterator_initializer_hook.iterator_initializer_fn = (
@@ -221,34 +269,83 @@ def get_eval_inputs(features, labels):
   return _eval_input_fn, iterator_initializer_hook
 
 
+def make_serving_input_fn():
+  """Returns serving input fn to receive tf.Example."""
+  feature_spec = tf.feature_column.make_parse_example_spec(
+      example_feature_columns().values())
+  return tf.estimator.export.build_parsing_serving_input_receiver_fn(
+      feature_spec)
+
+
+def make_transform_fn():
+  """Returns a transform_fn that converts features to dense Tensors."""
+
+  def _transform_fn(features, mode):
+    """Defines transform_fn."""
+    if mode == tf.estimator.ModeKeys.PREDICT:
+      # We expect tf.Example as input during serving. In this case, group_size
+      # must be set to 1.
+      if FLAGS.group_size != 1:
+        raise ValueError(
+            "group_size should be 1 to be able to export model, but get %s" %
+            FLAGS.group_size)
+      context_features, example_features = (
+          tfr.feature.encode_pointwise_features(
+              features=features,
+              context_feature_columns=None,
+              example_feature_columns=example_feature_columns(),
+              mode=mode,
+              scope="transform_layer"))
+    else:
+      context_features, example_features = tfr.feature.encode_listwise_features(
+          features=features,
+          context_feature_columns=None,
+          example_feature_columns=example_feature_columns(),
+          mode=mode,
+          scope="transform_layer")
+
+    return context_features, example_features
+
+  return _transform_fn
+
+
 def make_score_fn():
   """Returns a groupwise score fn to build `EstimatorSpec`."""
 
   def _score_fn(unused_context_features, group_features, mode, unused_params,
                 unused_config):
     """Defines the network to score a group of documents."""
-    with tf.name_scope("input_layer"):
+    with tf.compat.v1.name_scope("input_layer"):
       group_input = [
-          tf.layers.flatten(group_features[name])
+          tf.compat.v1.layers.flatten(group_features[name])
           for name in sorted(example_feature_columns())
       ]
       input_layer = tf.concat(group_input, 1)
-      tf.summary.scalar("input_sparsity", tf.nn.zero_fraction(input_layer))
-      tf.summary.scalar("input_max", tf.reduce_max(input_layer))
-      tf.summary.scalar("input_min", tf.reduce_min(input_layer))
+      tf.compat.v1.summary.scalar("input_sparsity",
+                                  tf.nn.zero_fraction(input_layer))
+      tf.compat.v1.summary.scalar("input_max",
+                                  tf.reduce_max(input_tensor=input_layer))
+      tf.compat.v1.summary.scalar("input_min",
+                                  tf.reduce_min(input_tensor=input_layer))
 
     is_training = (mode == tf.estimator.ModeKeys.TRAIN)
-    cur_layer = tf.layers.batch_normalization(input_layer, training=is_training)
+    cur_layer = tf.compat.v1.layers.batch_normalization(
+        input_layer, training=is_training)
     for i, layer_width in enumerate(int(d) for d in FLAGS.hidden_layer_dims):
-      cur_layer = tf.layers.dense(cur_layer, units=layer_width)
-      cur_layer = tf.layers.batch_normalization(cur_layer, training=is_training)
+      cur_layer = tf.compat.v1.layers.dense(cur_layer, units=layer_width)
+      cur_layer = tf.compat.v1.layers.batch_normalization(
+          cur_layer, training=is_training)
       cur_layer = tf.nn.relu(cur_layer)
-      tf.summary.scalar("fully_connected_{}_sparsity".format(i),
-                        tf.nn.zero_fraction(cur_layer))
-    cur_layer = tf.layers.dropout(
+      tf.compat.v1.summary.scalar("fully_connected_{}_sparsity".format(i),
+                                  tf.nn.zero_fraction(cur_layer))
+    cur_layer = tf.compat.v1.layers.dropout(
         cur_layer, rate=FLAGS.dropout_rate, training=is_training)
-    logits = tf.layers.dense(cur_layer, units=FLAGS.group_size)
-    return logits
+    logits = tf.compat.v1.layers.dense(cur_layer, units=FLAGS.group_size)
+    if _use_multi_head():
+      # Duplicate the logits for both heads.
+      return {_PRIMARY_HEAD: logits, _SECONDARY_HEAD: logits}
+    else:
+      return logits
 
   return _score_fn
 
@@ -285,24 +382,41 @@ def train_and_eval():
                                                 FLAGS.list_size)
   test_input_fn, test_hook = get_eval_inputs(features_test, labels_test)
 
+  optimizer = tf.compat.v1.train.AdagradOptimizer(
+      learning_rate=FLAGS.learning_rate)
+
   def _train_op_fn(loss):
     """Defines train op used in ranking head."""
-    return tf.contrib.layers.optimize_loss(
-        loss=loss,
-        global_step=tf.train.get_global_step(),
-        learning_rate=FLAGS.learning_rate,
-        optimizer="Adagrad")
+    update_ops = tf.compat.v1.get_collection(tf.compat.v1.GraphKeys.UPDATE_OPS)
+    minimize_op = optimizer.minimize(
+        loss=loss, global_step=tf.compat.v1.train.get_global_step())
+    train_op = tf.group([minimize_op, update_ops])
+    return train_op
 
-  ranking_head = tfr.head.create_ranking_head(
-      loss_fn=tfr.losses.make_loss_fn(FLAGS.loss),
-      eval_metric_fns=get_eval_metric_fns(),
-      train_op_fn=_train_op_fn)
+  if _use_multi_head():
+    primary_head = tfr.head.create_ranking_head(
+        loss_fn=tfr.losses.make_loss_fn(FLAGS.loss),
+        eval_metric_fns=get_eval_metric_fns(),
+        train_op_fn=_train_op_fn,
+        name=_PRIMARY_HEAD)
+    secondary_head = tfr.head.create_ranking_head(
+        loss_fn=tfr.losses.make_loss_fn(FLAGS.secondary_loss),
+        eval_metric_fns=get_eval_metric_fns(),
+        train_op_fn=_train_op_fn,
+        name=_SECONDARY_HEAD)
+    ranking_head = tfr.head.create_multi_ranking_head(
+        [primary_head, secondary_head], [1.0, FLAGS.secondary_loss_weight])
+  else:
+    ranking_head = tfr.head.create_ranking_head(
+        loss_fn=tfr.losses.make_loss_fn(FLAGS.loss),
+        eval_metric_fns=get_eval_metric_fns(),
+        train_op_fn=_train_op_fn)
 
   estimator = tf.estimator.Estimator(
       model_fn=tfr.model.make_groupwise_ranking_fn(
           group_score_fn=make_score_fn(),
           group_size=FLAGS.group_size,
-          transform_fn=None,
+          transform_fn=make_transform_fn(),
           ranking_head=ranking_head),
       config=tf.estimator.RunConfig(
           FLAGS.output_dir, save_checkpoints_steps=1000))
@@ -317,12 +431,24 @@ def train_and_eval():
       input_fn=train_input_fn,
       hooks=[train_hook],
       max_steps=FLAGS.num_train_steps)
-  vali_spec = tf.estimator.EvalSpec(
-      input_fn=vali_input_fn,
-      hooks=[vali_hook],
-      steps=1,
-      start_delay_secs=0,
-      throttle_secs=30)
+  # Export model to accept tf.Example when group_size = 1.
+  if FLAGS.group_size == 1:
+    vali_spec = tf.estimator.EvalSpec(
+        input_fn=vali_input_fn,
+        hooks=[vali_hook],
+        steps=1,
+        exporters=tf.estimator.LatestExporter(
+            "latest_exporter",
+            serving_input_receiver_fn=make_serving_input_fn()),
+        start_delay_secs=0,
+        throttle_secs=30)
+  else:
+    vali_spec = tf.estimator.EvalSpec(
+        input_fn=vali_input_fn,
+        hooks=[vali_hook],
+        steps=1,
+        start_delay_secs=0,
+        throttle_secs=30)
 
   # Train and validate
   tf.estimator.train_and_evaluate(estimator, train_spec, vali_spec)
@@ -332,7 +458,7 @@ def train_and_eval():
 
 
 def main(_):
-  tf.logging.set_verbosity(tf.logging.INFO)
+  tf.compat.v1.logging.set_verbosity(tf.compat.v1.logging.INFO)
 
   train_and_eval()
 
@@ -343,4 +469,4 @@ if __name__ == "__main__":
   flags.mark_flag_as_required("test_path")
   flags.mark_flag_as_required("output_dir")
 
-  tf.app.run()
+  tf.compat.v1.app.run()
